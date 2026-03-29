b04590bd5204425f7e0328dc9a3b5554e29257cb
smsibtainrn <smsibtainrn@gmail.com>
2026-03-29 20:24:52 +0500

    Section 9: Xylophone - Using Flutter and Dart Packages to Speed Up Development

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,48 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(MaterialApp(home: BallPage()));
+void main() => runApp(XylophoneApp());
 
-class BallPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      appBar: AppBar(
-        backgroundColor: Colors.blue.shade900,
-        title: Text('Ask Me Anything'),
+class XylophoneApp extends StatelessWidget {
+  final player = AudioPlayer();
+
+  void playSound(int soundNumber) async {
+    await player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildKey({required Color color, required int soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        ),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: const SizedBox(),
       ),
-      body: Ball(),
     );
   }
-}
-
-class Ball extends StatefulWidget {
-  @override
-  _BallState createState() => _BallState();
-}
-
-class _BallState extends State<Ball> {
-  int ballNumber = 1;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: TextButton(
-        onPressed: () {
-          setState(() {
-            ballNumber = Random().nextInt(5) + 1;
-          });
-        },
-        style: ButtonStyle(
-          foregroundColor: WidgetStateProperty.all(Colors.black),
-          overlayColor: WidgetStateProperty.all(Colors.transparent),
-          padding: WidgetStateProperty.all(
-            EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
           ),
         ),
-        child: Image.asset('images/ball$ballNumber.png'),
       ),
     );
   }
