d49179a6d8813898131a5a965ee97f8f5852b555
smsibtainrn <smsibtainrn@gmail.com>
2026-04-01 18:35:20 +0500

    Section 14: Boss Level Challenge 3 - Bitcoin Ticker

---
+++ lib/coin_data.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+
+const coinAPIURL = 'https://api-realtime.exrates.coinapi.io/v1/exchangerate';
+const apiKey = 'API_KEY_HERE';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    Map<String, String> cryptoPrices = {};
+    for (String crypto in cryptoList) {
+      String requestURL =
+          '$coinAPIURL/$crypto/$selectedCurrency';
+      http.Response response = await http.get(Uri.parse(requestURL),
+        headers: {
+          'Authorization': apiKey,
+        },);
+      if (response.statusCode == 200) {
+        var decodedData = jsonDecode(response.body);
+        double price = decodedData['rate'];
+        print('$crypto/$selectedCurrency - $price');
+        cryptoPrices[crypto] = price.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
+    }
+    return cryptoPrices;
+  }
+}
